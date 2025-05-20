72af362cb03436f81b035e877311e3a044e94d18
Auda <betaauda@gmail.com>
2025-05-20 08:08:12 +0200

    changes made

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -17,9 +17,12 @@ export async function sendReceiptEmail(requestId: string) {
 
     const transporter = nodemailer.createTransport({
       service: 'Gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASSWORD
       }
     });
 
